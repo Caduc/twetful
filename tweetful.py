663b4d6575ce059ed1fdf5b2e284f28cc80c0405
caduc <remaier@gmail.com>
2014-10-28 19:44:34 -0400

    first try with TL input

--- tweetful.py
+++ tweetful.py
@@ -1,15 +1,39 @@
 
+import authorization
+import json
+import requests
+import argparse
+import sys
+from urls import *
 
+def make_parser():
+    #ArgumentParser.add_subparsers
+    parser = argparse.ArgumentParser(description='ingest twitter API calls: timeline')
+    subparser = parser.add_subparser(dest="command", help="available commands")
+
+    #timeline
+    timeline_parse = subparsers.add_subparser("timeline", help = "Show Twitter Timeline")
 
 
 def main():
-	##  main function
-	pass
+    ##  main function
+    parser = make_parser()
+    arguments = parse.parse_args(sys.argv[0])
+    # Convert parsed arguments from Namespace to dictionary
+    arguments = vars(arguments)
+    command = arguments.pop("command")
+
+    auth = authorization.authorize()
+    
+    if command == "TL":
+        response = requests.get(TIMELINE_URL, auth=auth)
+        print json.dumps(response.json(), indent=4)
+
+    else:
+        print "geuss again sucka"
+        return    
 
 
 if __name__ == '__main__':
-	main()
+    main()
 
-def authorize():
-	
-	pass
